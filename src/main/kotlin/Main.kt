1d4dcbe34b0548644b60c0607151d2462e29420b
VSFe <klm03025@gmail.com>
2024-08-03 18:31:05 +0900

    feat: Step 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 fun main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	println("Logs from your program will appear here!")
-	ServerSocketManager.run()
+	ServerSocketManager.init()
 	println("accepted new connection")
+
+	while (true) {
+		var aa = ServerSocketManager.process()
+		println(aa)
+	}
 }
--- src/main/kotlin/ServerSocketManager.kt
+++ src/main/kotlin/ServerSocketManager.kt
@@ -3,8 +3,16 @@ import java.net.ServerSocket
 object ServerSocketManager {
 	private val serverSocket = ServerSocket(4221)
 
-	fun run() {
+	fun init() {
 		serverSocket.reuseAddress = true
-		serverSocket.accept()
+	}
+
+	fun process() {
+		val socket = serverSocket.accept()
+		val outputStream = socket.getOutputStream()
+
+		outputStream.write("HTTP/1.1 200 OK\r\n\r\n".toByteArray())
+		outputStream.flush()
+		outputStream.close()
 	}
 }
